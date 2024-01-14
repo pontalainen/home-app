d18c3d6329d5f6f0648b168d0957fa5953815c78
pontalainen <pontusolsson03@gmail.com>
2024-01-14 03:20:13 +0100

    Removed never used files

--- resources/js/plugins/vuetify.js
+++
@@ -1,8 +0,0 @@
-// import 'vuetify/styles'
-// import '@mdi/font/css/materialdesignicons.css'
-
-import { createVuetify } from 'vuetify';
-
-export default createVuetify(
-    // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-);
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@vue/cli-service";
-
-module.exports = defineConfig({
-    pluginOptions: {
-        vuetify: {
-            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-        }
-    }
-})
